ba168b9136e876b06170fc4af4751b22ef0e1b1a
somours <932223023@qq.com>
2018-12-13 14:15:25 +0800

    first commit init project

---
+++ components/public/header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        topbae
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        saearch-bar
+      </el-col>
+    </el-row>
+  </div>  
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+@import '@/assets/css/public/header/index.scss';
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container class="layout-default">
+    <el-header height="197px;">
+      <my-header/>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer height="1090%">
+      <my-footer/>
+    </el-footer>
+  </el-container>
+</template>
+<script>
+import MyHeader from '@/components/public/header/index.vue'
+import MyFooter from '@/components/public/footer/index.vue'
+export default {
+  components: {
+    MyHeader,
+    MyFooter
+  }
+}
+</script>
