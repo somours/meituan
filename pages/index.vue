ba9096089557b8ca24b2502c8be1ddf74836a4f3
somours <932223023@qq.com>
2018-12-13 17:59:36 +0800

    text carousel:beer:

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,14 @@
           class="button--grey">GitHub</a>
       </div>
     </div>
+    <div class="wraper">
+      <ul>
+        <li>第一条消息</li>
+        <li>第二条消息</li>
+        <li>第三条消息</li>
+        <li>第四条消息</li>
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -32,13 +40,35 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.wraper {
+  width: 400px;
+  height: 40px;
+  margin: 0 auto;
+  line-height: 40px;
+  background: #e6e6e6;
+  ul {
+    width: 100%;
+    overflow: hidden;
+    height: 100%;
+    position: relative;
+    li {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      z-index: 0;
+    }
+  }
+}
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // text-align: center;
 }
 
 .title {
